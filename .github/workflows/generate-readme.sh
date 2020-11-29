ccccd75c0b049df6244f3509a4cac4050e13afcb
Jordan Matelsky <j6k4m8@gmail.com>
2020-11-29 11:22:10 -0500

    Start github action

---
+++ .github/workflows/generate-readme.sh
@@ -0,0 +1,13 @@
+cd ../../
+
+cd posters
+mkdir -p thumbnails
+for f in *.pdf; do convert -thumbnail x400 -background white -alpha remove "$f" "thumbnails/${f%.pdf}.png"; done
+cd ..
+
+cd papers
+mkdir -p thumbnails
+for f in *.pdf; do convert -thumbnail x400 -background white -alpha remove "$f"[0] "thumbnails/${f%.pdf}.png"; done
+cd ..
+
+python3 .github/workflows/make-readme.py > README.md
--- .github/workflows/make-poster-thumbnails.sh
+++
@@ -1,1 +0,0 @@
-for f in ../../posters/*.pdf; do convert -thumbnail x300 -background white -alpha remove "$f" "thumb/${f%.pdf}.png"; done
