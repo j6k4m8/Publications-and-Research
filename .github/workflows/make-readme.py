03b240d68875e009b7fc64b0c431670e7d589358
Jordan Matelsky <j6k4m8@gmail.com>
2020-11-29 11:39:24 -0500

    debug: show git status after readme generator

--- .github/workflows/make-readme.py
+++ .github/workflows/make-readme.py
@@ -1,14 +1,13 @@
 import glob
 
-page = """
-
+page = """\
 # Publications & Research
 
-
-
-- [Google Scholar](https://scholar.google.com/citations?user=QgJ7CPUAAAAJ&hl=en)
-- [ResearchGate](https://www.researchgate.net/scientific-contributions/2132435999_Jordan_Matelsky)
-- [ORCID](https://orcid.org/0000-0002-9470-760X)
+<p align=center>
+<a href="https://scholar.google.com/citations?user=QgJ7CPUAAAAJ&hl=en"><img src="https://img.shields.io/badge/Google Scholar-4285F4?logo=Google+Scholar&style=for-the-badge&logoColor=black" /></a>
+<a href="https://www.researchgate.net/scientific-contributions/2132435999_Jordan_Matelsky"><img src="https://img.shields.io/badge/ResearchGate-00ccbb?logo=ResearchGate&style=for-the-badge&logoColor=black" /></a>
+<a href="https://orcid.org/0000-0002-9470-760X"><img src="https://img.shields.io/badge/ORCID-A6CE39?logo=ORCID&style=for-the-badge&logoColor=black" /></a>
+</p>
 
 For a complete CV, see [here](https://jordan.matelsky.com/resume/).
 
