918752fb2a7ca7076bace3b836f4fa8d4e18367a
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2021-03-13 19:41:58 -0500

    Update generate-readme.sh

--- .github/workflows/generate-readme.sh
+++ .github/workflows/generate-readme.sh
@@ -1,11 +1,12 @@
 cd posters
 mkdir -p thumbnails
-for f in *.pdf; do convert -thumbnail x400 -background white -alpha remove "$f" "thumbnails/${f%.pdf}.png"; done
+for f in *.pdf; do gs -dNOPAUSE -sDEVICE=pngalpha -r70 -sOutputFile=thumbnails/${f%.pdf}.png $f; done
+#for f in *.pdf; do convert -thumbnail x400 -background white -alpha remove "$f" "thumbnails/${f%.pdf}.png"; done
 cd ..
 
 cd papers
 mkdir -p thumbnails
-for f in *.pdf; do gs -dNOPAUSE -sDEVICE=pngalpha -r144 -sOutputFile=${f%.pdf}.png $f; done
+for f in *.pdf; do gs -dNOPAUSE -sDEVICE=pngalpha -r70 -sOutputFile=thumbnails/${f%.pdf}.png $f; done
 #for f in *.pdf; do convert -thumbnail x400 -background white -alpha remove "$f"[0] "thumbnails/${f%.pdf}.png"; done
 cd ..
 
